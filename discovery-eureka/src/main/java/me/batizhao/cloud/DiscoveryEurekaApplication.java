23219a04a0da82b98acf368d8c3b0af0d31410a9
Bati Zhao <zhaobati@gmail.com>
2017-02-24 08:55:44 +0800

    discovery eureka.

---
+++ discovery-eureka/src/main/java/me/batizhao/cloud/DiscoveryEurekaApplication.java
@@ -0,0 +1,14 @@
+package me.batizhao.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryEurekaApplication.class, args);
+	}
+}
