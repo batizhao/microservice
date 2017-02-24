7abff5b23349df46063ca9d2839fc95e94bb2660
Bati Zhao <zhaobati@gmail.com>
2017-02-24 15:24:49 +0800

    实现调用配置中心客户端。

--- oa-client-with-hystrix/src/main/java/me/batizhao/cloud/controller/OaClientController.java
+++ oa-client-with-hystrix/src/main/java/me/batizhao/cloud/controller/OaClientController.java
@@ -1,7 +1,10 @@
 package me.batizhao.cloud.controller;
 
+import me.batizhao.cloud.domain.User;
 import me.batizhao.cloud.service.OaClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2017/2/24
  */
 @RestController
+@RefreshScope
 public class OaClientController {
+
+    @Value("${profile}")
+    private String profile;
 
     @Autowired
     private OaClientService oaClientService;
 
     @GetMapping("/start")
-    public void findById() {
-        oaClientService.startProcess("batizhao", "zhaobati@gmail.com", "18917778888");
+    public User start() {
+        return oaClientService.startProcess("batizhao", "zhaobati@gmail.com", "18917778888");
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return this.profile;
     }
 
 }
